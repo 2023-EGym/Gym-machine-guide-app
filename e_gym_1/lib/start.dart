f6de86b28ca0541de2908849ceb022c99464d1a5
jonghun <qweasdfdsa11@naver.com>
2023-07-30 01:42:21 +0900

    모델 업그레이드 및 ui 화면 추가

--- e_gym_1/lib/start.dart
+++ e_gym_1/lib/start.dart
@@ -21,18 +21,12 @@ class StartPage extends StatelessWidget {
             Image(
                     image: AssetImage('assets/logo.png'),
                     width: 200, // 이미지의 너비 설정
-
                     ),
                               SizedBox(height: 250,),
-
             Text('손쉽게 카메라로 ',style : TextStyle(fontSize: 18)),
               Text('모든 운동 기구를 마스터하다',style : TextStyle(fontSize: 18)),
                               SizedBox(height: 70,),
-
-
             Text('E-GYM', style : TextStyle(fontSize: 40)),
-
-
 
           ]),
 
@@ -41,14 +35,16 @@ class StartPage extends StatelessWidget {
           child: Container(
             color: Color(0xff1E1651),
             child: SizedBox(
-              height: 60.0,
+              height: 80.0,
               child: TextButton(
                 onPressed: (){
                   Navigator.pushNamed(context, '/c');
                 },
                 child: Text("시작하기",
                   style: TextStyle(
+                    fontSize: 25,
                     color: Colors.white,
+
                   ),
                 ),
               ),
