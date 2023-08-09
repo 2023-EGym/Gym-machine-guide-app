8890ad088c33efa0a90fa1035f893cdf01cb19c0
Moon Ji In <moonjiin98@gmail.com>
2023-08-09 22:01:34 +0900

    refactor: 로딩 및 메인, 예시페이지 UI 수정

--- e_gym_1/lib/start.dart
+++ e_gym_1/lib/start.dart
@@ -15,40 +15,44 @@ class StartPage extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Column(children: [
-            SizedBox(height: 100,),
-            Image(
-                    image: AssetImage('assets/logo.png'),
-                    width: 200, // 이미지의 너비 설정
-                    ),
-                              SizedBox(height: 250,),
-            Text('손쉽게 카메라로 ',style : TextStyle(fontSize: 18)),
-              Text('모든 운동 기구를 마스터하다',style : TextStyle(fontSize: 18)),
-                              SizedBox(height: 70,),
-            Text('E-GYM', style : TextStyle(fontSize: 40)),
-
-          ]),
-
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.fromLTRB(10.0, 0, 0, 0),
+            child: Image.asset(
+              'assets/titleLogo.png',
+              width: 60,
+              height: 60,
             ),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            color: Color(0xff1E1651),
-            child: SizedBox(
-              height: 80.0,
-              child: TextButton(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/c');
-                },
-                child: Text("시작하기",
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-
+          ),
+          backgroundColor: Color(0xffFFFFFF),
+          toolbarHeight: 50,
+        ),
+        backgroundColor: Color(0xffFFFFFF),
+        body:
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('배우고 싶은 기구 촬영',
+                style: TextStyle(
+                  color: Color(0xff000000),
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                width: 300.0,
+                height: 300.0,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: TextButton(onPressed: (){
+                    Navigator.pushNamed(context, '/c');
+                  },
+                    child: Image.asset('assets/picbutton.png'),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
         ),
       ),
